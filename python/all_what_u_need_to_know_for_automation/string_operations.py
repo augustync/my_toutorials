fae84ead79d69654189d3fbf7837cfe7f28e2dee
Augustyn Chmiel <augustyn.chmiel@acoustic.com>
2020-06-12 11:10:23 +0100

    adding new chapter of my tuto

--- python/all_what_u_need_to_know_for_automation/string_operations.py
+++ python/all_what_u_need_to_know_for_automation/string_operations.py
@@ -79,7 +79,10 @@ print(f"Checking string name: [{name[:-2]}] if is only alphabethic : [{name[:-2]
 print(f"Checking string name: [{name}] if is only numeric : [{name.isnumeric()}]")
 print(f"Checking string name: [{name[-2:]}] if is only numeric : [{name[-2:].isnumeric()}]")
 print(f"To get a list of functions and descriptions for string operations:\n".title())
-help(str)
+print("help(str)".center(30))
+################################
+# uncomment if needed to see help on string.
+# help(str)
 
 
 # String join, center and zfill(zero fill)
@@ -95,3 +98,56 @@ print(f"Print variable [{name}] in the format centre: [{name.center(50)}]")
 print(f"Print variable [{name}] in the format zfill: [{name.zfill(50)}]")
 print(f"Print variable [{name}] in the format centre(30).zfill(10): [{name.center(30).zfill(10)}]")
 print(f"Print variable [{name}] in the format zfill(10).centre(30): [{name.zfill(10).center(30)}]")
+
+
+################################################################
+#### strip,split operations
+print("strip,split operations on string".title())
+str = 'Master of Disasterrrrr'
+master_str = 'Master of Disaster Master'
+str_with_spaces = ' Testing Spaces with start and end characters      '
+print(f"Print string [{str}] with spaces using strip operation [{str.strip()}]")
+print(f"Print string [{str_with_spaces}] with spaces using split operation[{str_with_spaces.strip()}]")
+print(f"Print string [{str}] with letter 'M' using strip operation [{str.strip('M')}]")
+print(f"Print string [{str}] with letter 'm' using strip operation [{str.strip('m')}]")
+print(f"Print string [{str}] with letter 'r' using strip operation [{str.strip('r')}]")
+print(f"Print string [{str}] with letter 'a' using strip operation [{str.strip('a')}]")
+print(f"Print string [{master_str}] with Word 'Master' using strip operation [{master_str.strip('Master')}]")
+print(f"Print string [{master_str}] with Word 'Master' using strip operation on left side [{master_str.lstrip('Master')}]")
+print(f"Print string [{master_str}] with Word 'Master' using strip operation on right side [{master_str.rstrip('Master')}]")
+print(f"Print string [{str}] with split operation by spaces [{str.split()}]")
+print(f"Print string [{str}] with split operation by word 'of' [{str.split('of')}]")
+print(f"Print string [{str}] with split operation by letter 'a' [{str.split('a')}]")
+
+#### ##############################
+#### count,index and find opearations on strings
+print("count,index and find operations on strings".title())
+print(f"Print string [{str}] and count letter 'a' [{str.count('a')}]")
+print(f"Print string [{str}] and count letter 'of' [{str.count('of')}]")
+print(f"Print string [{str}] and index letter 'a' [{str.index('a')}]")
+num = 2
+print(f"Print string [{str}] and index letter 'a' after index [{num}] position [{num + 1}] :: [{str.index('a', num)}]")
+print(f"Print string [{str}] and index letter 'of' [{str.index('of')}]")
+print(f"Print string [{str}] and find letter 'a' [{str.find('a')}]")
+print(f"Print string [{str}] and find letter 'a' after index [{num}] position [{num + 1}] :: [{str.find('a', num)}]")
+print(f"Print string [{str}] and find letter 'Disaster' [{str.find('Disaster')}]")
+print(f"Print string [{str}] and find letter 'Disasterrrrr' [{str.find('Disasterrrrr')}]")
+
+################################################################################################
+###### Practice: Display given string at left/right/center of a line in title format
+just = 50
+print(f"Display string [{str}] at center of a line:")
+print(str.center(just))
+print(f"Display string [{str}] at left side of a line:")
+print(str.ljust(just))
+print(f"Display string [{str}] at right side of a line:")
+print(str.rjust(just))
+
+import os
+t_w = os.get_terminal_size().columns
+print(f"Display string [{str}] at center of a line:")
+print(str.center(t_w))
+print(f"Display string [{str}] at left side of a line:")
+print(str.ljust(t_w))
+print(f"Display string [{str}] at right side of a line:")
+print(str.rjust(t_w))
